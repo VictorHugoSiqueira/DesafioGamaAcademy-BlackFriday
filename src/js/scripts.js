467a34f8473af36e12673f9f79977bf883b5789c
Victor Hugo Fernandes <huggofernandes@hotmail.com>
2021-07-17 20:32:45 -0300

    conclusão landing page Desafio Gama Academy

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.2 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
 const form = document.getElementById('form')
 
 form.addEventListener('signup', (e) => {
