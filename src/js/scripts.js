e9b597999d65fdda8e42285247774932f47c049c
Victor Hugo Fernandes <huggofernandes@hotmail.com>
2021-07-17 21:15:59 -0300

    editando index.pug

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form')
 
-form.addEventListener('signup', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   let nome = document.getElementById('nome').value;
   let email = document.getElementById('email').value;
