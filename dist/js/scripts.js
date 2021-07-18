3cc0f23003091b705ecd888bab75a53ff52b0b72
Victor Hugo Fernandes <huggofernandes@hotmail.com>
2021-07-17 22:06:02 -0300

    melhorias estéticas *css

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,31 +3,5 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
-const form = document.getElementById('form')
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let nome = document.getElementById('nome').value;
-  let email = document.getElementById('email').value;
-  let data = {
-    nome,
-    email,
-  }
-  let convertData = JSON.stringify(data);
-
-  localStorage.setItem('lead', convertData)
-
-  let content = document.getElementById('content')
-
-  let carregando = `<p>carregando...</p>`
-
-  let pronto = `<p>pronto</p>`
-
-  content.innerHTML = carregando
-
-
-  setTimeout(() => {
-    content.innerHTML = pronto
-  }, 1000)
-
-})
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
